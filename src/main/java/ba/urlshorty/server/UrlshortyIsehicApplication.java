f791671000ae5bc2849e76ecc24be11e349a163a
sheha <i.sheeha@gmail.com>
2017-02-18 15:41:14 +0100

    init commit

---
+++ src/main/java/ba/urlshorty/server/UrlshortyIsehicApplication.java
@@ -0,0 +1,12 @@
+package ba.urlshorty.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortyIsehicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortyIsehicApplication.class, args);
+	}
+}
---
+++ src/test/java/ba/urlshorty/server/UrlshortyIsehicApplicationTests.java
@@ -0,0 +1,16 @@
+package ba.urlshorty.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrlshortyIsehicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
